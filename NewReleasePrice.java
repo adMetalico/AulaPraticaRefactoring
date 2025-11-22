21974df08ad83c33062dc03ac8990498eba7652a
Adriel <adriel.alegrette@gmail.com>
2025-11-22 17:26:22 -0300

    Commit 15
    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -8,5 +8,9 @@ public class NewReleasePrice extends Price {
       return daysRented * 3;
    }
 
+   public int getFrequentRenterPoints(int daysRented) {
+      return (daysRented > 1) ? 2 : 1;
+   }
+
 }
 
